b1abc7d8b7dce4114b95cf4f77455e9e05bce819
mazerty <mazerty@gmail.com>
2021-04-27 09:28:37 +0200

    correction des fichiers sql

--- resources/create_table.sql
+++ resources/create_table.sql
@@ -1,22 +1,17 @@
 CREATE TABLE album
 (
-    id           int unsigned NOT NULL AUTO_INCREMENT,
-    title        varchar(255) DEFAULT NULL,
-    artist_name  varchar(255) DEFAULT NULL,
-    release_year int unsigned DEFAULT NULL,
-    PRIMARY KEY (id)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+    id           int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title        varchar(255),
+    artist_name  varchar(255),
+    release_year int
+);
 
 CREATE TABLE track
 (
-    id       int unsigned NOT NULL,
-    id_album int unsigned NOT NULL,
-    title    varchar(255) DEFAULT NULL,
-    duration int          DEFAULT NULL,
+    id       int NOT NULL,
+    id_album int NOT NULL,
+    title    varchar(255),
+    duration int,
     PRIMARY KEY (id, id_album),
-    CONSTRAINT fk_track_album FOREIGN KEY (id_album) REFERENCES album (id)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci;
+    CONSTRAINT fk_album FOREIGN KEY (id_album) REFERENCES album (id)
+);
--- resources/requests.sql
+++ resources/requests.sql
@@ -1,28 +1,28 @@
-# a
+-- a
 SELECT *
 FROM album;
 
-# b
+-- b
 SELECT DISTINCT artist_name
 FROM album;
 
-# c
+-- c
 SELECT release_year
 FROM album
 WHERE title = 'The Black Market';
 
-# d
+-- d
 SELECT COUNT(*)
-FROM `track`
+FROM track
 WHERE duration < 120;
 
-# e
+-- e
 SELECT track.id, track.title
 FROM track
          INNER JOIN album on track.id_album = album.id
 WHERE album.title = 'Toto IV';
 
-# f
+-- f
 SELECT album.title, COUNT(*)
 FROM album
          JOIN track ON album.id = track.id_album
